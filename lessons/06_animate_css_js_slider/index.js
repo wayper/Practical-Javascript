2c2ccd677f13d5b6fbc81372e1b9c56c33efd211
Dmitry Ivanov <wayper.plenitude@gmail.com>
2020-01-25 21:48:39 +0200

    animate_css

--- lessons/06_animate_css_js_slider/index.js
+++ lessons/06_animate_css_js_slider/index.js
@@ -1,12 +1,27 @@
+document.querySelector('button').onclick = mySlider;
 let sliderImages = document.querySelectorAll('.slider-box img');
 let k = 0;
 
 function mySlider() {
-    console.log(k);
-    console.log(sliderImages);
-    let img = sliderImages[k];
-    img.style.display = 'block';
-    img.classList.add('animated', 'rollIn');
+    if (k < sliderImages.length) {
+        console.log(k);
+        let img = sliderImages[k];
+        img.style.display = 'block';
+        img.classList.add('animated', 'rollIn');
+        img.addEventListener('animationend', ()=>{
+            img.classList.add('animated', 'rollOut', 'delay-1s');
+        });
+        k++;
+    } else {
+        startNewSlider();
+    }
 }
 
-mySlider();
+function startNewSlider() {
+    sliderImages.forEach((element) => {
+        element.classList.remove('animated', 'rollIn', 'rollOut', 'delay-1s');
+        element.style.display = 'none';
+    });
+    k = 0;
+    mySlider();
+}
